87c3cac37f308a788d0f6319c81689d53f098e08
vudkhoa <vudinhkhoaowl@gmail.com>
2024-11-27 17:07:31 +0700

    Update

--- 02_Obstacle_Course/ObstacleCourse/Assets/Mover.cs
+++ 02_Obstacle_Course/ObstacleCourse/Assets/Mover.cs
@@ -2,7 +2,13 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+
+    // these variables change our movement speed
+    // [SerializeField] float xValue = 0;
+    // [SerializeField] float yValue = 0.02f;
+    // [SerializeField] float zValue = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float moveSpeed = 8f;
     void Start()
     {
         // transform.Translate(0.01f, 0, 0); --> Di chuyển một lần lúc ban đầu
@@ -12,6 +18,8 @@ public class NewMonoBehaviourScript : MonoBehaviour
     void Update()
     {
         // += 0.01
-        transform.Translate(0.01f, 0, 0);
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical")   * Time.deltaTime * moveSpeed;
+        transform.Translate(xValue, 0, zValue);
     }
 }
